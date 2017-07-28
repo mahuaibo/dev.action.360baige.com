6c27fe5ebce7b02a2d82af48ac99a33a6a489d1e
mahuaibo <mahuaibo@360baige.com>
2017-07-28 13:03:13 +0800

    ma

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import (
 	_ "dev.action.360baige.com/database"
 	_ "dev.action.360baige.com/rpc/server"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
+	beego.Get("/", func(ctx *context.Context) {
+
+		ctx.WriteString("ok")
+	})
 	beego.Run()
 }
