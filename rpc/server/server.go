64dad0bb5de6b71fc899500234dc8a2ece9e6672
mahuaibo <mahuaibo@360baige.com>
2017-08-18 00:30:50 +0800

    ma

--- rpc/server/server.go
+++ rpc/server/server.go
@@ -14,12 +14,13 @@ import (
 	"dev.action.360baige.com/actions/order"
 	"dev.action.360baige.com/actions/application"
 	"dev.action.360baige.com/actions/message"
-	//"dev.action.360baige.com/actions/attendance"
-	//"dev.action.360baige.com/actions/card"
-	//"dev.action.360baige.com/actions/machine"
+	"dev.action.360baige.com/actions/card"
+	"dev.action.360baige.com/actions/machine"
 	"dev.action.360baige.com/actions/personnel"
 	"dev.action.360baige.com/actions/schoolfee"
-	"time"
+	"dev.action.360baige.com/actions/website"
+	"dev.action.360baige.com/actions/authority"
+	//"dev.action.360baige.com/actions/attendance"
 )
 
 func init() {
@@ -28,45 +29,62 @@ func init() {
 	log.SetLogger(logs.AdapterConsole)
 	log.Debug("server run start")
 	// 服务
-	services := map[string]interface{}{
-
-		"User":         &user.UserAction{},
-		"UserPosition": &user.UserPositionAction{},
-
-		"Company": &company.CompanyAction{},
-
-		"Logger": &logger.LoggerAction{},
-
-		"City": &city.CityAction{},
-
-		"Account":     &account.AccountAction{},
-		"AccountItem": &account.AccountItemAction{},
-		"Transaction": &account.TransactionAction{},
-
-		"Order": &order.OrderAction{},
-
-		"Application":    &application.ApplicationAction{},
-		"ApplicationTpl": &application.ApplicationTplAction{},
-		"MessageTemp":    &message.MessageTempAction{},
-
-		"Project": &schoolfee.ProjectAction{},
-		"Record":  &schoolfee.RecordAction{},
-
-		////
-		//"AttendanceGroup":       &attendance.AttendanceGroupAction{},
-		//"AttendanceRecord":      &attendance.AttendanceRecordAction{},
-		//"AttendanceSetup":       &attendance.AttendanceSetupAction{},
-		//"AttendanceShift":       &attendance.AttendanceShiftAction{},
-		//"AttendanceShiftItem":   &attendance.AttendanceShiftItemAction{},
-		//"AttendanceShiftRecord": &attendance.AttendanceShiftRecordAction{},
-		//"Card": &card.CardAction{},
-		//"Machine": &machine.MachineAction{},
-		////
-
-		"Person":          &personnel.PersonAction{},
-		"PersonRelation":  &personnel.PersonRelationAction{},
-		"PersonStructure": &personnel.PersonStructureAction{},
-		"Structure":       &personnel.StructureAction{},
+	services := map[string]map[string]interface{}{
+		"User": map[string]interface{}{
+			"User":         &user.UserAction{},
+			"UserPosition": &user.UserPositionAction{},
+		},
+		"Company": map[string]interface{}{
+			"Company": &company.CompanyAction{},
+		},
+		"Logger": map[string]interface{}{
+			"Logger": &logger.LoggerAction{},
+		},
+		"City": map[string]interface{}{
+			"City": &city.CityAction{},
+		},
+		"Account": map[string]interface{}{
+			"Account":     &account.AccountAction{},
+			"AccountItem": &account.AccountItemAction{},
+			"Transaction": &account.TransactionAction{},
+		},
+		"Order": map[string]interface{}{
+			"Order": &order.OrderAction{},
+		},
+		"Application": map[string]interface{}{
+			"Application":    &application.ApplicationAction{},
+			"ApplicationTpl": &application.ApplicationTplAction{},
+		},
+		"Message": map[string]interface{}{
+			"MessageSend":  &message.MessageSendAction{},
+			"MessageTemp":  &message.MessageTempAction{},
+			"MessageTotal": &message.MessageTotalAction{},
+		},
+		"Schoolfee": map[string]interface{}{
+			"Project": &schoolfee.ProjectAction{},
+			"Record":  &schoolfee.RecordAction{},
+		},
+		"Personnel": map[string]interface{}{
+			"Person":          &personnel.PersonAction{},
+			"PersonRelation":  &personnel.PersonRelationAction{},
+			"PersonStructure": &personnel.PersonStructureAction{},
+			"Structure":       &personnel.StructureAction{},
+		},
+		"Card": map[string]interface{}{
+			"Card": &card.CardAction{},
+		},
+		"Machine": map[string]interface{}{
+			"Machine": &machine.MachineAction{},
+		},
+		"Website": map[string]interface{}{
+			"Menu":     &website.MenuAction{},
+			"Material": &website.MaterialAction{},
+		},
+		"Authority": map[string]interface{}{
+			"Gather":       &authority.GatherAction{},
+			"GatherItem":   &authority.GatherItemAction{},
+			"PersonGather": &authority.PersonGatherAction{},
+		},
 	}
 	go register(services)
 	log.Debug("server run end")
@@ -75,33 +93,43 @@ func init() {
 /**
  * 注册服务
  */
-func register(services map[string]interface{}) {
+func register(services map[string]map[string]interface{}) {
 
 	etcdServerRegisterAddr := beego.AppConfig.String("RpcEtcdURL")
 	serverRegisterAddr := beego.AppConfig.String("RpcServer")
-	servs := make([]string, len(services))
-	//for serName, _ := range services {
-	//	servs = append(servs, serName)
+
+	//var servs []string
+	//for _, group_services := range services {
+	//	for name, _ := range group_services {
+	//		servs = append(servs, name)
+	//	}
 	//}
+	var length = 0
+	for _, group_services := range services {
+		length += len(group_services)
+	}
+	servs := make([]string, length)
 
 	//EtcdV3RegisterPlugin EtcdRegisterPlugin time.Minute
-	rplugin := &plugin.EtcdRegisterPlugin{
-		ServiceAddress: "tcp@" + serverRegisterAddr,
-		EtcdServers:    []string{etcdServerRegisterAddr },
-		BasePath:       "/rpcx",
-		Metrics:        metrics.NewRegistry(),
-		Services:       servs,
-		UpdateInterval: time.Minute,
-		//UpdateIntervalInSec: 60,
+	rplugin := &plugin.EtcdV3RegisterPlugin{
+		ServiceAddress:      "tcp@" + serverRegisterAddr,
+		EtcdServers:         []string{etcdServerRegisterAddr },
+		BasePath:            "/rpcx",
+		Metrics:             metrics.NewRegistry(),
+		Services:            servs,
+		UpdateIntervalInSec: 5*60,
 	}
+
 	rplugin.Start()
 	server := rpcx.NewServer()
 	server.PluginContainer.Add(rplugin)
 	server.PluginContainer.Add(plugin.NewAliasPlugin())
 
 	//注册 s
-	for name, serv := range services {
-		server.RegisterName(name, serv, "weight=1&m=devops")
+	for _, group_services := range services {
+		for name, serv := range group_services {
+			server.RegisterName(name, serv, "weight=1&m=devops")
+		}
 	}
 	//注册 e
 	server.Serve("tcp", serverRegisterAddr)
